64fc9055dc01ae0df2ab6859855967d3276ba543
MikhailTravin <78166836+MikhailTravin@users.noreply.github.com>
2026-04-05 21:26:16 +0300

    Add files via upload

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1578,3 +1578,63 @@ function tabs() {
   }
 }
 tabs();
+
+//========================================================================================================================================================
+
+//Яндекс карта
+const map = document.querySelector('#map1');
+
+if (map) {
+  let myMap = null;
+
+  ymaps.ready(init);
+
+  function init() {
+    myMap = new ymaps.Map('map1', {
+      center: [55.699781, 37.619423],
+      zoom: 8,
+      controls: ['zoomControl'],
+      behaviors: ['drag']
+    }, {
+      searchControlProvider: 'yandex#search'
+    });
+
+    myMap.geoObjects
+      .add(new ymaps.Placemark([55.694843, 37.435023], {
+        iconCaption: 'Очень длиннный, но невероятно интересный текст',
+        iconColor: '#0c8ce9',
+      }))
+      .add(new ymaps.Placemark([55.831903, 37.411961], {
+        iconCaption: 'Очень длиннный, но невероятно интересный текст',
+        iconColor: '#0c8ce9',
+      }))
+      .add(new ymaps.Placemark([55.763338, 37.565466], {
+        iconCaption: 'Очень длиннный, но невероятно интересный текст',
+        iconColor: '#0c8ce9',
+      }))
+      .add(new ymaps.Placemark([55.763338, 37.565466], {
+        iconCaption: 'Очень длиннный, но невероятно интересный текст',
+        iconColor: '#0c8ce9',
+      }))
+      .add(new ymaps.Placemark([55.744522, 37.616378], {
+        iconCaption: 'Очень длиннный, но невероятно интересный текст',
+        iconColor: '#0c8ce9',
+      }));
+  }
+
+  function updateMapSize() {
+    if (myMap) {
+      myMap.container.fitToViewport();
+    }
+  }
+
+  document.addEventListener('click', function (e) {
+    const tabTitle = e.target.closest('[data-tabs-title]');
+    if (tabTitle) {
+      const mapContainer = document.querySelector('#map1');
+      if (mapContainer && !mapContainer.hidden && mapContainer.offsetParent !== null) {
+        updateMapSize();
+      }
+    }
+  });
+}
